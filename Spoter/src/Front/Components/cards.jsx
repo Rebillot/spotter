0b6951b87a2090d7af550bf0f7320e70455ead44
Rebillot <jonathan.rebillot@gmail.com>
2023-09-07 12:48:42 -0300

    working on it

---
+++ Spoter/src/Front/Components/cards.jsx
@@ -0,0 +1,24 @@
+import React from "react";  
+
+
+export default function Cards() {
+    return (
+        <div class="card" style="width: 18rem;">
+  <img src="https://flyingmag.sfo3.digitaloceanspaces.com/flyingma/wp-content/uploads/2021/11/10185810/C-124-2.jpg" class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+    )
+    
+}
